28545fc14dd8969edb0d60fff6f1a6a18bcc5b08
Leon <leon_1122@live.cn>
2020-03-25 10:52:49 +0800

    modify user page noncloud

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,92 +1,82 @@
 getApp();
 
-var t = wx.cloud.database(), e = t.collection("user"), n = !1;
+var Const = require('../../utils/const.js');
+var userId = wx.getStorageSync('userId');
 
 Page({
     data: {
-        userInfo: null,
-        showOpenSettingBtn: !1
+        userInfo: null
     },
     onShow: function() {
         this.getUser();
     },
-    getUser: function(t) {
-        var e = this;
-        wx.cloud.callFunction({
-            name: "getUser"
-        }).then(function(o) {
-            o.result.data.length > 0 ? (n = !0, e.setData({
-                userInfo: o.result.data[0]
-            }, t && t())) : n = !1;
-        });
-    },
-    onGetUserInfo: function(o) {
-        var s = this;
-        if (!n) {
-            var a = o.detail.userInfo;
-            a.moneyBag = 0, a.bagList = [], a.createTime = t.serverDate(), 
-            e.add({
-                data: a
-            }).then(function(t) {
-                n = !0, s.setData({
-                    userInfo: a
-                });
-            }).catch(function(t) {
-                console.error(t);
-            });
+    login: function(o) {
+        if (userId) {
+            return;
         }
-    },
-    openSetting: function() {
-        var t = this;
-        wx.getSetting({
-            success: function(e) {
-                e.authSetting["scope.address"] && t.chooseAddress(function() {
-                    t.setData({
-                        showOpenSettingBtn: !1
-                    });
-                });
-            }
-        });
-    },
-    onAddressManage: function() {
-        this.data.userInfo ? this.chooseAddress() : wx.showToast({
-            title: "请先登录",
-            icon: "none"
-        });
-    },
-    chooseAddress: function(t) {
-        var e = this;
-        wx.chooseAddress({
-            success: function(n) {
-                wx.showLoading({
-                    title: "加载中"
-                });
-                var o = "".concat(n.provinceName).concat(n.cityName).concat(n.countyName).concat(n.detailInfo, ",").concat(n.userName, ",").concat(n.telNumber);
-                console.log(n);
-                wx.cloud.callFunction({
-                    name: "setAddress",
+
+        var code, encryptedData = o.detail.encryptedData, iv = o.detail.iv;
+        console.log(o.detail.encryptedData);
+        console.log(o.detail.iv);
+        
+        wx.login({
+            success: function(res) {
+                console.log(res.code);
+                code = res.code;
+
+                wx.request({
+                    url: Const.getUrl() + 'login',
+                    method: 'POST',
                     data: {
-                        address: o,
-                        addressObject: n,
-                        phoneNumber: n.telNumber
-                    }
-                }).then(function(n) {
-                    e.getUser(function() {
-                        wx.hideLoading();
-                    }), t && t();
-                }).catch(function(t) {
-                    console.error(t);
-                });
-            },
-            fail: function(t) {
-                wx.getSetting({
-                    success: function(t) {
-                        t.authSetting["scope.address"] || e.setData({
-                            showOpenSettingBtn: !0
+                        code,
+                        encryptedData,
+                        iv
+                    },
+                    success: function(res) {
+                        console.log(res.data);
+                        userId = res.data.userId;
+                        wx.setStorageSync('userId', userId);
+                    },
+                    fail: function(e) {
+                        console.error(e);
+                        wx.showToast({
+                            title: "出错啦！请联系客服",
+                            icon: "none"
                         });
                     }
                 });
             }
-        });
+        })
+    },
+    getUser: function(t) {
+        var that = this;
+        if (userId) {
+            wx.request({
+                url: Const.getUrl() + 'getUserInfo',
+                method: 'GET',
+                data: {
+                    userId
+                },
+                success: function(res) {
+                    console.log(res.data);
+
+                    if (res.data.userInfo) {
+                        that.setData({
+                            userInfo: res.data.userInfo
+                        });
+                    } else {
+                        wx.removeStorageSync('userId');
+                        userId = null;
+                    }
+                },
+                fail: function(e) {
+                    console.error(e);
+                    wx.showToast({
+                        title: "出错啦！请联系客服",
+                        icon: "none"
+                    });
+                }
+            });
+        }
     }
 });
---
+++ miniprogram/utils/const.js
@@ -0,0 +1,8 @@
+module.exports = {
+    getUrl: getUrl,
+}
+
+// 接口URL
+function getUrl() {
+    return 'http://localhost:1337/api/v1/wx/';
+}
